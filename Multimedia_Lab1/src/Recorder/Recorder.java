e4d7d9f07a66e4bdeb4a1967a609f7236380bee8
MarcDahlem <mardal-2@student.ltu.se>
2012-12-04 16:42:11 +0100

    first client implementation with oggmux and theoraenc

--- Multimedia_Lab1/src/Recorder/Recorder.java
+++ Multimedia_Lab1/src/Recorder/Recorder.java
@@ -9,7 +9,6 @@ import org.gstreamer.ElementFactory;
 import org.gstreamer.GhostPad;
 import org.gstreamer.Pad;
 import org.gstreamer.Pipeline;
-import org.gstreamer.event.EOSEvent;
 import org.gstreamer.swt.VideoComponent;
 
 /**
@@ -34,12 +33,7 @@ public class Recorder {
 	 * a boolean value that descibes, if the recorder is capturing video at the moment
 	 */
 	private boolean isRecording;
-
-	private Bin firstBin;
-	private Element switcher;
-
-	private Bin currentRecordBin;
-
+	
 	/**The default constructor for this recorder. It needs a VideoComponent to get the video playpack sink
 	 * 
 	 * @param vid component containing the playback sink for the recorder
@@ -53,69 +47,21 @@ public class Recorder {
 		// requires a installed "motion" plugin in gstreamer
 
 		Bin sourceBin = createSourceBin();
-		Element tee = ElementFactory.make("tee", "Tee split buffer");
-		Element switcher = ElementFactory.make("valve", "Switcher for recording");
-		Element record_queue= ElementFactory.make ("queue", "recording queue");
-		switcher.set("drop", false);
-		Bin fakeRecordBin = createFakeRecordBin();
 		Bin playBin = createPlayBin(vid);
 
-		firstBin.addMany(sourceBin, tee, record_queue, switcher, playBin);
-		Element.linkMany(sourceBin, tee);
-		Element.linkMany(tee, record_queue, switcher);
-		Element.linkMany(tee, playBin);
-		// add a ghost pad, so that the bin is accessible from the outside
-		Pad staticSourcePad = switcher.getStaticPad("src");
-		GhostPad ghost = new GhostPad("src", staticSourcePad);
-		firstBin.addPad(ghost);
+		firstBin.addMany(sourceBin, playBin);
+		Element.linkMany(sourceBin, playBin);
+		pipe.addMany(firstBin);
 
-		pipe.addMany(firstBin, fakeRecordBin);
-		Element.linkMany(firstBin, fakeRecordBin);
-
-		this.currentRecordBin=fakeRecordBin;
-		this.switcher=switcher;
-		this.firstBin = firstBin;
 		this.pipe=pipe;
-	}
-
-	private Bin createFakeRecordBin() {
-		Bin recordBin=new Bin("Recorder fake subpipe");
-		Element fakeSink = ElementFactory.make("fakesink","Recorder fakesink");
-		recordBin.addMany(fakeSink);
-		Pad staticSourcePad = fakeSink.getStaticPad("sink");
-		GhostPad ghost = new GhostPad("sink", staticSourcePad);
-		recordBin.addPad(ghost);
-
-		return recordBin;
-	}
-
-	private Bin createRealRecordBin(String fileName) {
-		// assert fileName != null
-		Bin recordBin = new Bin("Recorder subpipe");
-		//enc = ElementFactory.make("x264enc", "avi Encoder");
-		Element enc = ElementFactory.make("theoraenc", "Encoder ogg");
-		//mux = ElementFactory.make("avimux", "avi Muxer");
-		Element mux = ElementFactory.make("oggmux", "Ogg Muxer");
-		// fileSink
-		Element fileSink = ElementFactory.make("filesink", "File Sink");
-		fileSink.set("location", fileName);
-		recordBin.addMany(enc, mux, fileSink);
-		Element.linkMany(enc, mux, fileSink);
-		// add a ghost pad, so that the bin is accessible from the outside
-		Pad staticSourcePad = enc.getStaticPad("sink");
-		GhostPad ghost = new GhostPad("sink", staticSourcePad);
-		recordBin.addPad(ghost);
-
-		return recordBin;
 	}
 
 	private Bin createPlayBin(VideoComponent vid) {
 		Bin playBin = new Bin("playback");
-		Element vidSink = vid.getElement();
-		vidSink.setName("SWTVideo");
+		Element vidSink = ElementFactory.make ("tcpclientsink", "tcp client sink");
+		vidSink.set("port", 5000);
 
 		Element play_queue= ElementFactory.make ("queue", "playback queue");
-		play_queue.set("leaky", 1);
 
 		playBin.addMany(play_queue, vidSink);
 		Element.linkMany(play_queue, vidSink);
@@ -131,14 +77,16 @@ public class Recorder {
 		Bin sourceBin = new Bin("source");
 		Element src = ElementFactory.make("v4l2src", "video capturing source");
 		//Element motionDetection = ElementFactory.make("motion", "motion detection");
-		sourceBin.addMany(src);
+		Element enc= ElementFactory.make ("theoraenc", "theory encoder");
+		Element mux = ElementFactory.make("oggmux", "ogg muxer");
+		sourceBin.addMany(src, enc, mux);
 
 		//link the motion detector and the webcam
 		//sourceBin.addMany(src, motionDetection);
 		// Element.linkMany(src, motionDetection);
 
 		// add a ghost pad, so that the bin is accessible from the outside
-		Pad staticSourcePad = src.getStaticPad("src");
+		Pad staticSourcePad = mux.getStaticPad("src");
 		GhostPad ghost = new GhostPad("src", staticSourcePad);
 		sourceBin.addPad(ghost);
 		//return the created sourceBin
@@ -158,9 +106,7 @@ public class Recorder {
 	 */
 	public void stopRec() {
 		if (this.isRecording()) {
-			//recording => stop recording
-			Bin newFakeBin = this.createFakeRecordBin();
-			this.changeRecordBin(newFakeBin);
+			//TODO
 		}
 	}
 
@@ -179,28 +125,9 @@ public class Recorder {
 
 		// check if it is already recording
 		if (!this.isRecording()) {
-			//not recroding --> start record;
-			//create recorder bin to set the new filename
-			Bin newRecordBin = this.createRealRecordBin(fileName);
-			changeRecordBin(newRecordBin);
+			//TODO
 		}
 
-	}
-
-	private void changeRecordBin(Bin newRecordBin) {
-		this.switcher.set("drop", true);
-	    this.currentRecordBin.sendEvent(new EOSEvent());
-		this.pipe.remove(this.currentRecordBin);
-		this.currentRecordBin.stop();
-		
-		this.pipe.addMany(newRecordBin);
-		Element.linkMany(this.firstBin, newRecordBin);
-		newRecordBin.play();
-		this.switcher.set("drop", false);
-
-		this.currentRecordBin = newRecordBin;
-		//change the isRecording status
-		this.isRecording=!this.isRecording;
 	}
 
 	/**
