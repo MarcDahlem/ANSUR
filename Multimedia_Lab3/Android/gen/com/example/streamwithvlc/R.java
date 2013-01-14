78c498e8b38ebde60c140249e0f12764934d279e
Jon Martin Mikalsen <overmind_zerg6@hotmail.com>
2013-01-14 13:37:14 +0100

    Set up for notification

--- Multimedia_Lab3/Android/gen/com/example/streamwithvlc/R.java
+++ Multimedia_Lab3/Android/gen/com/example/streamwithvlc/R.java
@@ -19,6 +19,7 @@ public final class R {
         public static final int bg=0x7f020000;
         public static final int bg_orange=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int motion_detected=0x7f020003;
     }
     public static final class id {
         public static final int ServerhostNameField=0x7f080007;
