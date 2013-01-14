e8bbefa86e2a4743b2e5ef1a38e9f1d7bd50e108
Jon Martin Mikalsen <overmind_zerg6@hotmail.com>
2013-01-14 15:04:28 +0100

    Updated gcmintentservice

--- Multimedia_Lab3/Android/src/com/example/streamwithvlc/MainActivity.java
+++ Multimedia_Lab3/Android/src/com/example/streamwithvlc/MainActivity.java
@@ -203,45 +203,7 @@ public class MainActivity extends Activity {
 			break;
 		}
 	}
-
-	public void launchApplication(){
-		PackageManager packageManager = getApplicationContext().getPackageManager();
-		Intent applicationIntent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER);
-
-		// Verify clock implementation
-		// Use package explorer in google play store to find package name and activity name
-		String clockImpls[][] = {
-				{"VLC Player", "org.videolan.vlc.android", "org.videolan.vlc.android.MainActivity"},
-				//{"HTC Alarm Clock", "com.htc.android.worldclock", "com.htc.android.worldclock.WorldClockTabControl" },
-				{"Standard Alarm Clock", "com.android.deskclock", "com.android.deskclock.AlarmClock"},
-				{"Froyo Nexus Alarm Clock", "com.google.android.deskclock", "com.android.deskclock.DeskClock"},
-				{"Moto Blur Alarm Clock", "com.motorola.blur.alarmclock",  "com.motorola.blur.alarmclock.AlarmClock"},
-				{"Samsung Galaxy Clock", "com.sec.android.app.clockpackage","com.sec.android.app.clockpackage.ClockPackage"}
-		};
-
-		boolean foundClockImpl = false;
-
-		for(int i=0; i<clockImpls.length; i++) {
-			String vendor = clockImpls[i][0];
-			String packageName = clockImpls[i][1];
-			String className = clockImpls[i][2];
-			try {
-				ComponentName cn = new ComponentName(packageName, className);
-				ActivityInfo aInfo = packageManager.getActivityInfo(cn, PackageManager.GET_META_DATA);
-				applicationIntent.setComponent(cn);
-				Log.d("Debug", "Found " + vendor + " --> " + packageName + "/" + className);
-				foundClockImpl = true;
-			} catch (NameNotFoundException e) {
-				Log.d("Debug", vendor + " does not exists");
-			}
-		}
-
-		if (foundClockImpl) {
-			applicationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			startActivity(applicationIntent);
-		}
-	}
-
+	
 	@Override
 	protected void onDestroy() {
 		// destroy 
